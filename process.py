0b5614c98fd20a45963939e35b4d4563a79a8b70
yang yang <yanyang729@gmail.com>
2017-04-26 00:43:35 -0400

    get 10 data set

---
+++ process.py
@@ -0,0 +1,283 @@
+import re
+import nltk
+import string
+from nltk.stem import WordNetLemmatizer
+from collections import Counter
+import pandas as pd
+import time
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+import warnings
+warnings.filterwarnings('ignore')
+stopword_list = nltk.corpus.stopwords.words('english')
+wnl = WordNetLemmatizer()
+
+
+def get_topn_tags_transform(path, topn, tag):
+    """
+    read cleaned data and transform them into one tag per row
+    1. get top n tags
+    2. expand row
+    3. to boolean
+    4. aggregate by content
+    """
+    df = pd.read_csv(path,quotechar='|',sep=',',header=None)
+    df.columns = ['title','body','tags']
+    merged = [ title + ' ' + body for title, body in zip(df.title,df.body)]
+    df_merged = pd.DataFrame({'content':merged,'tags':df.tags})
+    df_merged.tags = df_merged.tags.apply(lambda x: x.replace('<','').split('>')[:-1])
+    df_transfromed = pd.DataFrame(df_merged.tags.tolist(),index=df_merged.content).stack().reset_index()[['content',0]]
+    df_transfromed.columns = ['content','tags']
+    top_tags = Counter(df_transfromed.tags).most_common()[:topn]
+    top_n_tags = [fktag for fktag, num in top_tags]
+    df_filtered = df_transfromed[df_transfromed.tags.apply(lambda x: x in set(top_n_tags))]
+    df_filtered.tags = [int(bool) for bool in df_filtered.tags == tag]
+    df_filtered.columns = ['content','is_{}'.format(tag)]
+    rslt = df_filtered.groupby('content')['is_{}'.format(tag)].agg(['sum']).reset_index()
+    rslt.columns = ['content','is_{}'.format(tag)]
+    return rslt, top_n_tags
+
+
+def tokenize_text(text):
+    tokens = nltk.word_tokenize(text)
+    tokens = [token.strip() for token in tokens]
+    return tokens
+
+
+def expand_contractions(text, contraction_mapping):
+    contractions_pattern = re.compile('({})'.format('|'.join(contraction_mapping.keys())),
+                                      flags=re.IGNORECASE | re.DOTALL)
+
+    def expand_match(contraction):
+        match = contraction.group(0)
+        first_char = match[0]
+        expanded_contraction = contraction_mapping.get(match) \
+            if contraction_mapping.get(match) \
+            else contraction_mapping.get(match.lower())
+        expanded_contraction = first_char + expanded_contraction[1:]
+        return expanded_contraction
+
+    expanded_text = contractions_pattern.sub(expand_match, text)
+    expanded_text = re.sub("'", "", expanded_text)
+    return expanded_text
+
+
+from pattern.en import tag
+from nltk.corpus import wordnet as wn
+
+
+# Annotate text tokens with POS tags
+def pos_tag_text(text):
+    def penn_to_wn_tags(pos_tag):
+        if pos_tag.startswith('J'):
+            return wn.ADJ
+        elif pos_tag.startswith('V'):
+            return wn.VERB
+        elif pos_tag.startswith('N'):
+            return wn.NOUN
+        elif pos_tag.startswith('R'):
+            return wn.ADV
+        else:
+            return None
+
+    tagged_text = tag(text)
+    tagged_lower_text = [(word.lower(), penn_to_wn_tags(pos_tag))
+                         for word, pos_tag in
+                         tagged_text]
+    return tagged_lower_text
+
+
+# lemmatize text based on POS tags
+def lemmatize_text(text):
+    pos_tagged_text = pos_tag_text(text)
+    lemmatized_tokens = [wnl.lemmatize(word, pos_tag) if pos_tag
+                         else word
+                         for word, pos_tag in pos_tagged_text]
+    lemmatized_text = ' '.join(lemmatized_tokens)
+    return lemmatized_text
+
+
+def remove_special_characters(text):
+    tokens = tokenize_text(text)
+    pattern = re.compile('[{}]'.format(re.escape(string.punctuation)))
+    filtered_tokens = filter(None, [pattern.sub('', token) for token in tokens])
+    filtered_text = ' '.join(filtered_tokens)
+    return filtered_text
+
+
+def remove_stopwords(text):
+    tokens = tokenize_text(text)
+    filtered_tokens = [token for token in tokens if token not in stopword_list]
+    filtered_text = ' '.join(filtered_tokens)
+    return filtered_text
+
+
+def normalize_corpus(corpus, tokenize=False):
+    normalized_corpus = []
+    for text in corpus:
+        text = expand_contractions(text, CONTRACTION_MAP)
+        text = lemmatize_text(text)
+        text = remove_special_characters(text)
+        text = remove_stopwords(text)
+        normalized_corpus.append(text)
+        if tokenize:
+            text = tokenize_text(text)
+            normalized_corpus.append(text)
+
+    return normalized_corpus
+
+
+CONTRACTION_MAP = {
+    "ain't": "is not",
+    "aren't": "are not",
+    "can't": "cannot",
+    "can't've": "cannot have",
+    "'cause": "because",
+    "could've": "could have",
+    "couldn't": "could not",
+    "couldn't've": "could not have",
+    "didn't": "did not",
+    "doesn't": "does not",
+    "don't": "do not",
+    "hadn't": "had not",
+    "hadn't've": "had not have",
+    "hasn't": "has not",
+    "haven't": "have not",
+    "he'd": "he would",
+    "he'd've": "he would have",
+    "he'll": "he will",
+    "he'll've": "he he will have",
+    "he's": "he is",
+    "how'd": "how did",
+    "how'd'y": "how do you",
+    "how'll": "how will",
+    "how's": "how is",
+    "I'd": "I would",
+    "I'd've": "I would have",
+    "I'll": "I will",
+    "I'll've": "I will have",
+    "I'm": "I am",
+    "I've": "I have",
+    "i'd": "i would",
+    "i'd've": "i would have",
+    "i'll": "i will",
+    "i'll've": "i will have",
+    "i'm": "i am",
+    "i've": "i have",
+    "isn't": "is not",
+    "it'd": "it would",
+    "it'd've": "it would have",
+    "it'll": "it will",
+    "it'll've": "it will have",
+    "it's": "it is",
+    "let's": "let us",
+    "ma'am": "madam",
+    "mayn't": "may not",
+    "might've": "might have",
+    "mightn't": "might not",
+    "mightn't've": "might not have",
+    "must've": "must have",
+    "mustn't": "must not",
+    "mustn't've": "must not have",
+    "needn't": "need not",
+    "needn't've": "need not have",
+    "o'clock": "of the clock",
+    "oughtn't": "ought not",
+    "oughtn't've": "ought not have",
+    "shan't": "shall not",
+    "sha'n't": "shall not",
+    "shan't've": "shall not have",
+    "she'd": "she would",
+    "she'd've": "she would have",
+    "she'll": "she will",
+    "she'll've": "she will have",
+    "she's": "she is",
+    "should've": "should have",
+    "shouldn't": "should not",
+    "shouldn't've": "should not have",
+    "so've": "so have",
+    "so's": "so as",
+    "that'd": "that would",
+    "that'd've": "that would have",
+    "that's": "that is",
+    "there'd": "there would",
+    "there'd've": "there would have",
+    "there's": "there is",
+    "they'd": "they would",
+    "they'd've": "they would have",
+    "they'll": "they will",
+    "they'll've": "they will have",
+    "they're": "they are",
+    "they've": "they have",
+    "to've": "to have",
+    "wasn't": "was not",
+    "we'd": "we would",
+    "we'd've": "we would have",
+    "we'll": "we will",
+    "we'll've": "we will have",
+    "we're": "we are",
+    "we've": "we have",
+    "weren't": "were not",
+    "what'll": "what will",
+    "what'll've": "what will have",
+    "what're": "what are",
+    "what's": "what is",
+    "what've": "what have",
+    "when's": "when is",
+    "when've": "when have",
+    "where'd": "where did",
+    "where's": "where is",
+    "where've": "where have",
+    "who'll": "who will",
+    "who'll've": "who will have",
+    "who's": "who is",
+    "who've": "who have",
+    "why's": "why is",
+    "why've": "why have",
+    "will've": "will have",
+    "won't": "will not",
+    "won't've": "will not have",
+    "would've": "would have",
+    "wouldn't": "would not",
+    "wouldn't've": "would not have",
+    "y'all": "you all",
+    "y'all'd": "you all would",
+    "y'all'd've": "you all would have",
+    "y'all're": "you all are",
+    "y'all've": "you all have",
+    "you'd": "you would",
+    "you'd've": "you would have",
+    "you'll": "you will",
+    "you'll've": "you will have",
+    "you're": "you are",
+    "you've": "you have"
+}
+
+if __name__ == '__main__':
+    # do the pre process here
+    tic = time.time()
+    DATA_PATH = './data/processed/stack_ds_4_9_2017 .csv'
+    TAGS = ['javascript',
+             'java',
+             'android',
+             'php',
+             'python',
+             'c#',
+             'html',
+             'jquery',
+             'ios',
+             'css']
+    NUM_TAGS_CONSIDERED = 10
+
+    print('start transform data')
+    for i in range(10):
+        WHICH_TAG = TAGS[i]
+        df, _ = get_topn_tags_transform(DATA_PATH,NUM_TAGS_CONSIDERED,WHICH_TAG)
+        corpus = [text for text in df.content]
+        corpus = normalize_corpus(corpus)
+        df.content = corpus
+        df.to_csv('./data/processed/10sets/is_{}.csv'.format(WHICH_TAG), index=False)
+
+    toc = time.time()
+    print('{}s used'.format(str(int(toc-tic))))
