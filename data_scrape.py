88fa08e5651644b712a055f8869e3c2fb2450d4a
gaoji <gaojianj96@gmail.com>
2017-05-09 19:40:37 -0400

    Scrape documentation

--- data_scrape.py
+++ data_scrape.py
@@ -46,14 +46,14 @@ class DataScraper:
 
     def __init__(self,size=10000, from_date=1451606400, to_date=1483228800):
         """
-        Init the class DataScraper
-        :type size: int
+        Init the class DataScraper. Use method scrape to start scrape.
+        :type size: int default 10000
         :type from_date: long
         :type to_date: long
-        :param size: items of scrape data. Default 100000
-        :param from_date: The start of creation date of questions. Type is datetime. timestamp. Default 1451606400
-        :param to_date: The end of creation date of questions. Type is datetime. timestamp. Default 1483228800
-        """
+        :param size: API requests of scrape data. Default 100000
+        :param from_date: The start of creation date of questions. Type is datetime. timestamp. Default 1451606400 means 2016-01-01
+        :param to_date: The end of creation date of questions. Type is datetime. timestamp. Default 1483228800 means 2017-01-01
+-       """
         self.data_size = size
         self.fromDate = from_date
         self.toDate = to_date
@@ -63,8 +63,8 @@ class DataScraper:
         """Return a list(max 100) of items
         Scrape one API link
         :param page: the number of page, start from 1, default 1
-        :param from_date: The start of creation date of questions. Type is datetime. timestamp. Default 1451606400
-        :param to_date: The end of creation date of questions. Type is datetime. timestamp. Default 1483228800
+        :param from_date: The start of creation date of questions. Type is datetime. timestamp. Default 1451606400 means 2016-01-01
+        :param to_date: The end of creation date of questions. Type is datetime. timestamp. Default 1483228800 means 2017-01-01
         :returns: has_more: Argument from API return
                     final_reults: A list of list of items
         """
@@ -89,6 +89,15 @@ class DataScraper:
 
     @staticmethod
     def load_sets(size=10000, from_date=1451606400, to_date=1483228800):
+        """Return a lists of items
+        :type size: int default 10000
+        :type from_date: long
+        :type to_date: long
+        :param size: API requests of scrape data. Default 100000
+        :param from_date: The start of creation date of questions. Type is datetime. timestamp. Default 1451606400 means 2016-01-01
+        :param to_date: The end of creation date of questions. Type is datetime. timestamp. Default 1483228800 means 2017-01-01
+        :return: df_set: lists of items. Maxim= size*100 items
+        """
         for_size = size / 100
         if size % 100 == 0:
             for_size -= 1
@@ -105,16 +114,22 @@ class DataScraper:
         return df_set
 
     def scrape(self):
+        """Retrun dataframe of items with columns: "title", "body","time","tags"
+        Start scrape data. Export dataframe with columns: "title", "body","tags"
+        :return: A dataframe of items with columns: "title", "body","time","tags"
+        """
         dataset=DataScraper.load_sets(self.data_size,self.fromDate,self.toDate)
         df=pd.DataFrame(dataset,columns=["title", "body","time","tags"])
-        df=df[["title", "body","tags"]]
-        df.to_csv("./data/processed/scape_data.csv",header=False,index=False,encoding="utf8")
+        df_out=df[["title", "body","tags"]]
+        df_out.to_csv("./data/processed/scape_data.csv",header=False,index=False,encoding="utf8")
         return df
 
+#Test
 if __name__ == '__main__':
     start="2016-01-01"
     end="2017-01-01"
     convertDT=lambda dt:int((dateutil.parser.parse(dt) - datetime.datetime(1970, 1, 1)).total_seconds())
     scraper=DataScraper(size=100000,from_date=convertDT(start),to_date=convertDT(end))
-    scraper.scrape()
+    df_full=scraper.scrape()
+    df_fullo_csv("./data/scape_data_time.csv",header=False,index=False,encoding="utf8")
 
